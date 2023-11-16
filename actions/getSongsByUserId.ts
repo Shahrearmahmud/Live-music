c18ed44906073230305eda2b0f806ee003709908
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2023-11-16 17:09:13 +0600

    SearchSongs

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -1,15 +1,25 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+
 import { Song } from "@/types";
 
-const getSongs = async (): Promise<Song[]> => {
+const getSongsByUserId = async (): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies,
   });
 
+  const { data: sessionData, error: sessionError } =
+    await supabase.auth.getSession();
+
+  if (sessionError) {
+    console.log(sessionError.message);
+    return [];
+  }
+
   const { data, error } = await supabase
     .from("songs")
     .select("*")
+    .eq("user_id", sessionData.session?.user.id)
     .order("created_at", { ascending: false });
 
   if (error) {
@@ -19,4 +29,4 @@ const getSongs = async (): Promise<Song[]> => {
   return (data as any) || [];
 };
 
-export default getSongs;
+export default getSongsByUserId;
